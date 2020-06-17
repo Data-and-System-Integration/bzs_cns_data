049788104744bb2a542e74f7dbfccc47fe62a017
dmandloi <>
2020-06-17 07:31:53 -0400

    {/src/main/java/com/priyanka/bzs_cns_data/data_user_defined_multi.java}

--- src/main/java/com/priyanka/bzs_cns_data/data_user_defined_multi.java
+++ src/main/java/com/priyanka/bzs_cns_data/data_user_defined_multi.java
@@ -11,6 +11,8 @@ public class data_user_defined_multi implements java.io.Serializable {
 	private java.lang.Long code_count;
 	private java.util.List<java.lang.Long> user_defined_codes;
 
+	private java.lang.Double test1;
+
 	public data_user_defined_multi() {
 	}
 
@@ -31,10 +33,20 @@ public class data_user_defined_multi implements java.io.Serializable {
 		this.user_defined_codes = user_defined_codes;
 	}
 
+	public java.lang.Double getTest1() {
+		return this.test1;
+	}
+
+	public void setTest1(java.lang.Double test1) {
+		this.test1 = test1;
+	}
+
 	public data_user_defined_multi(java.lang.Long code_count,
-			java.util.List<java.lang.Long> user_defined_codes) {
+			java.util.List<java.lang.Long> user_defined_codes,
+			java.lang.Double test1) {
 		this.code_count = code_count;
 		this.user_defined_codes = user_defined_codes;
+		this.test1 = test1;
 	}
 
 }
